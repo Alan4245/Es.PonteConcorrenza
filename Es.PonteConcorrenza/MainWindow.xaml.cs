691195ad0329051ce5670b78813d51d6cdf93bc4
Alan4245 <alanminecraft77@gmail.com>
2021-02-16 18:41:05 +0100

    Implementato il thread per il semaforo e aggiunta le auto

--- Es.PonteConcorrenza/MainWindow.xaml.cs
+++ Es.PonteConcorrenza/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Es.PonteConcorrenza
 {
@@ -20,9 +21,97 @@ namespace Es.PonteConcorrenza
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int counterSX;
+        public static int counterDX;
+        
         public MainWindow()
         {
             InitializeComponent();
+            MainWindow.counterSX = 0;
+            MainWindow.counterDX = 0;
+            Thread t1 = new Thread(new ThreadStart(AggiornaSemafori));
+            t1.Start();
+        }
+
+        private void btnAggiungiAutoSX_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                counterSX += int.Parse(txtBoxInputSX.Text);
+                txtBoxInputSX.Clear();
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show("Errore durante l'aggiunta di auto: " + ex.Message);
+
+            }
+
+        }
+
+        private void btnAggiungiAutoDX_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                counterDX += int.Parse(txtBoxInputDX.Text);
+                txtBoxInputDX.Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Errore durante l'aggiunta di auto: " + ex.Message);
+
+            }
+
+        }
+
+        public void AggiornaSemafori()
+        {
+            Uri semaforoROSSOuri = new Uri("semaforoROSSO.png", UriKind.Relative);
+            Uri semaforoGIALLOuri = new Uri("semaforoGIALLO.png", UriKind.Relative);
+            Uri semaforoVERDEuri = new Uri("semaforoVERDE.png", UriKind.Relative);
+            ImageSource semaforoROSSO = new BitmapImage(semaforoROSSOuri);
+            ImageSource semaforoGIALLO = new BitmapImage(semaforoGIALLOuri);
+            ImageSource semaforoVERDE = new BitmapImage(semaforoVERDEuri);
+            semaforoROSSO.Freeze();
+            semaforoGIALLO.Freeze();
+            semaforoVERDE.Freeze();
+
+            while (true)
+            {
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgSemaforoSX.Source = semaforoROSSO;
+                    imgSemaforoDX.Source = semaforoVERDE;                   
+                }));
+
+                Thread.Sleep(30000);
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgSemaforoDX.Source = semaforoGIALLO;
+                }));
+                
+                Thread.Sleep(4000);
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgSemaforoSX.Source = semaforoVERDE;
+                    imgSemaforoDX.Source = semaforoROSSO;
+                }));
+
+                Thread.Sleep(30000);
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgSemaforoSX.Source = semaforoGIALLO;
+                }));
+                
+                Thread.Sleep(4000);
+            }
         }
     }
 }
