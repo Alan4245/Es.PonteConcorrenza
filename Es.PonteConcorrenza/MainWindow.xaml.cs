d1a156d5d29dbbf45d7118350f9ebb2c11e93d6f
Alan4245 <alanminecraft77@gmail.com>
2021-02-16 20:22:46 +0100

    Implementata la partenza delle auto a SX, fixati alcuni problemi di accesso ai thread

--- Es.PonteConcorrenza/MainWindow.xaml.cs
+++ Es.PonteConcorrenza/MainWindow.xaml.cs
@@ -23,14 +23,30 @@ namespace Es.PonteConcorrenza
     {
         public static int counterSX;
         public static int counterDX;
-        
+        public static Uri semaforoROSSOuri = new Uri("semaforoROSSO.png", UriKind.Relative);
+        public static Uri semaforoGIALLOuri = new Uri("semaforoGIALLO.png", UriKind.Relative);
+        public static Uri semaforoVERDEuri = new Uri("semaforoVERDE.png", UriKind.Relative);
+        public static ImageSource semaforoROSSO = new BitmapImage(semaforoROSSOuri);
+        public static ImageSource semaforoGIALLO = new BitmapImage(semaforoGIALLOuri);
+        public static ImageSource semaforoVERDE = new BitmapImage(semaforoVERDEuri);
+        public static object x = new object();
+        public static bool procediSX = false;
+        public static bool procediDX = false;
+
         public MainWindow()
         {
             InitializeComponent();
             MainWindow.counterSX = 0;
             MainWindow.counterDX = 0;
+            semaforoROSSO.Freeze();
+            semaforoGIALLO.Freeze();
+            semaforoVERDE.Freeze();
             Thread t1 = new Thread(new ThreadStart(AggiornaSemafori));
+            Thread t2 = new Thread(new ThreadStart(AggiornaAutoInLista));
+            Thread t3 = new Thread(new ThreadStart(MovimentoAutoVersoDestra));
             t1.Start();
+            t2.Start();
+            t3.Start();
         }
 
         private void btnAggiungiAutoSX_Click(object sender, RoutedEventArgs e)
@@ -68,16 +84,7 @@ namespace Es.PonteConcorrenza
         }
 
         public void AggiornaSemafori()
-        {
-            Uri semaforoROSSOuri = new Uri("semaforoROSSO.png", UriKind.Relative);
-            Uri semaforoGIALLOuri = new Uri("semaforoGIALLO.png", UriKind.Relative);
-            Uri semaforoVERDEuri = new Uri("semaforoVERDE.png", UriKind.Relative);
-            ImageSource semaforoROSSO = new BitmapImage(semaforoROSSOuri);
-            ImageSource semaforoGIALLO = new BitmapImage(semaforoGIALLOuri);
-            ImageSource semaforoVERDE = new BitmapImage(semaforoVERDEuri);
-            semaforoROSSO.Freeze();
-            semaforoGIALLO.Freeze();
-            semaforoVERDE.Freeze();
+        {                    
 
             while (true)
             {
@@ -85,7 +92,9 @@ namespace Es.PonteConcorrenza
                 this.Dispatcher.BeginInvoke(new Action(() =>
                 {
                     imgSemaforoSX.Source = semaforoROSSO;
-                    imgSemaforoDX.Source = semaforoVERDE;                   
+                    imgSemaforoDX.Source = semaforoVERDE;
+                    procediDX = true;
+                    procediSX = false;
                 }));
 
                 Thread.Sleep(30000);
@@ -93,6 +102,7 @@ namespace Es.PonteConcorrenza
                 this.Dispatcher.BeginInvoke(new Action(() =>
                 {
                     imgSemaforoDX.Source = semaforoGIALLO;
+                    procediDX = false;
                 }));
                 
                 Thread.Sleep(4000);
@@ -100,7 +110,8 @@ namespace Es.PonteConcorrenza
                 this.Dispatcher.BeginInvoke(new Action(() =>
                 {
                     imgSemaforoSX.Source = semaforoVERDE;
-                    imgSemaforoDX.Source = semaforoROSSO;
+                    imgSemaforoDX.Source = semaforoROSSO;                   
+                    procediSX = true;
                 }));
 
                 Thread.Sleep(30000);
@@ -108,10 +119,68 @@ namespace Es.PonteConcorrenza
                 this.Dispatcher.BeginInvoke(new Action(() =>
                 {
                     imgSemaforoSX.Source = semaforoGIALLO;
+                    procediSX = false;
                 }));
                 
                 Thread.Sleep(4000);
             }
+        }
+
+        public void AggiornaAutoInLista()
+        {
+            while (true)
+            {
+
+                Thread.Sleep(500);
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    lblCounterSX.Content = counterSX;
+                }));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    lblCounterDX.Content = counterDX;
+                }));
+            }
+        }
+
+        public void MovimentoAutoVersoDestra()
+        {
+            
+            while (true)
+            {
+                if(MainWindow.counterSX > 0)
+                {                 
+                    if(procediSX)
+                    {
+                        lock (x)
+                        {
+                           
+                            int posIniziale = 100;
+                            do
+                            {
+
+                                posIniziale++;
+                                this.Dispatcher.BeginInvoke(new Action(() =>
+                                {
+                                    imgSX.Margin = new Thickness(posIniziale, 125, 0, 0);
+                                }));
+                                Thread.Sleep(8);
+
+                            } while (posIniziale < 470);
+                            
+                            posIniziale = 100;
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                imgSX.Margin = new Thickness(posIniziale, 125, 0, 0);
+                            }));
+
+                            counterSX--;
+                        }
+                    }
+                }
+
+            }
+
         }
     }
 }
